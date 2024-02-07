dee82f620f4a80c2a4a37162b06f54e85be8920e
jonathan-luscap <152494215+jonathan-luscap@users.noreply.github.com>
2024-02-07 14:44:17 +0100

    Update app.php

---
+++ bootstrap/app.php
@@ -0,0 +1,5 @@
+<?php
+require_once '../config/database.php';
+require_once '../app/persistences/product.php';
+require_once '../app/persistences/cart.php';
+require_once '../app/persistences/command.php';

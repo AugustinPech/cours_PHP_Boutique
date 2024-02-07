c30cb218f2587eebb8165cee7fe51fda2fee076d
jonathan-luscap <152494215+jonathan-luscap@users.noreply.github.com>
2024-02-07 16:03:17 +0100

    Update product.php

--- app/persistances/product.php
+++ app/persistances/product.php
@@ -7,3 +7,11 @@ function getProduct(PDO $pdoConnection, $productId){
     return $statement->fetch(PDO::FETCH_ASSOC);
 }
 
+function getProducts($pdo, $numberOfProducts)
+{
+    $query = "select * from products order by priceTTC limit $numberOfProducts";
+
+    $statement = $pdo->query($query);
+    $outputNumberOfProducts = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $outputNumberOfProducts;
+}

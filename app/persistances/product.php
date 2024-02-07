79e916da269ff415f6c95e314580886abb8d93df
jonathan-luscap <152494215+jonathan-luscap@users.noreply.github.com>
2024-02-07 15:19:03 +0100

    Create product.php

---
+++ app/persistances/product.php
@@ -0,0 +1,9 @@
+<?php
+
+function getProduct(PDO $pdoConnection, $productId){
+    $statement = $pdoConnection->query("SELECT p.id, p.name, p.description, p.allTaxesIncludedPrice AS price, p.stock
+                        FROM products p
+                        WHERE p.id = $productId");
+    return $statement->fetch(PDO::FETCH_ASSOC);
+}
+
